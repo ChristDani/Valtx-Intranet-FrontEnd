0e2eda887ca9053f3029645e3cb51520e28fc066
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-02-28 10:57:47 -0500

    Se añadió vista del login

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,3 +1,41 @@
 export default function AuthenticationPage(){
     
+    return(
+        <>
+        <div className="flex flex-col justify-center min-h-full px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                {/**<img className="mx-auto h-10 w-auto" src="" alt="valtx"></img> */}
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 global-main-text">Valtx</h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" action="#" method="POST">
+                <div>
+                    <label className="block font-medium text-sm leading-6 text-gray-900">Correo:</label>
+                    <div className="mt-2">
+                    <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="flex items-center justify-between">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">Constraseña:</label>
+                    <div className="text-sm">
+                        <a href="#" className="font-semibold global-main-text">Recuperar contraseña</a>
+                    </div>
+                    </div>
+                    <div className="mt-2">
+                    <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit" className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#284488] focus-visible:outline global-main-button">Ingresar</button>
+                </div>
+                </form>
+            </div>
+        </div>
+        </>
+    );
+
 }
