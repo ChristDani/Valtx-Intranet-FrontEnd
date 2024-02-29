a893474277fb782f24c49c1b3104c8b34b35c410
Christian Daniel Campaña Díaz <christiand0393@gmail.com>
2024-02-29 12:44:09 -0500

    login

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,40 +1,51 @@
-export default function AuthenticationPage(){
-    
-    return(
+import axios from 'axios';
+
+export default function AuthenticationPage() {
+
+    const login = async () => {
+        const res = await axios.post('http://localhost:4000/api/v1/seguridad/login', {
+            email: "10000000",
+            password: "FA585D89C851DD338A70DCF535AA2A92FEE7836DD6AFF1226583E88E0996293F16BC009C652826E0FC5C706695A03CDDCE372F139EFF4D13959DA6F1F5D3EABE"
+        })
+
+        console.log(res.data.Message);
+    };
+
+    return (
         <>
-        <div className="flex flex-col justify-center min-h-full px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                {/**<img className="mx-auto h-10 w-auto" src="" alt="valtx"></img> */}
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 global-main-text">Valtx</h2>
+            <div className="flex flex-col justify-center min-h-full px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    {/**<img className="mx-auto h-10 w-auto" src="" alt="valtx"></img> */}
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 global-main-text">Valtx</h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" action="#" method="POST" onSubmit={login}>
+                        <div>
+                            <label className="block font-medium text-sm leading-6 text-gray-900">Documento:</label>
+                            <div className="mt-2">
+                                <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label className="block text-sm font-medium leading-6 text-gray-900">Constraseña:</label>
+                                <div className="text-sm">
+                                    <a href="#" className="font-semibold global-main-text">Recuperar contraseña</a>
+                                </div>
+                            </div>
+                            <div className="mt-2">
+                                <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                            </div>
+                        </div>
+
+                        <div>
+                            <button type="submit" className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#284488] focus-visible:outline global-main-button">Ingresar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" action="#" method="POST">
-                <div>
-                    <label className="block font-medium text-sm leading-6 text-gray-900">Correo:</label>
-                    <div className="mt-2">
-                    <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-                    </div>
-                </div>
-
-                <div>
-                    <div className="flex items-center justify-between">
-                    <label className="block text-sm font-medium leading-6 text-gray-900">Constraseña:</label>
-                    <div className="text-sm">
-                        <a href="#" className="font-semibold global-main-text">Recuperar contraseña</a>
-                    </div>
-                    </div>
-                    <div className="mt-2">
-                    <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-                    </div>
-                </div>
-
-                <div>
-                    <button type="submit" className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#284488] focus-visible:outline global-main-button">Ingresar</button>
-                </div>
-                </form>
-            </div>
-        </div>
         </>
     );
 
