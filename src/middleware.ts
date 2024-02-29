6d2e91e256519b22a0973f7f913c8d889d46c596
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-02-29 17:39:43 -0500

    creación del middleware

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+'use-client';
+
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server'
+import { jwtVerify  } from 'jsonwebtoken'
+
+export async function middleware(req:NextRequest){
+
+    const token = req.cookies.get('token');
+    
+    if(token === null){
+        return NextResponse.redirect(new URL('authentication', req.url));
+    }
+
+    try{
+
+        const {payload} = await jwtVerify(token, new TextEncoder().encode("secret"));
+        return NextResponse.next();
+
+    } catch(error){
+
+        return NextResponse.redirect(new URL('authentication', req.url));
+        
+    }
+
+    return NextResponse.next();
+
+}
+
+export const config = {
+    matcher:['/intranet']
+}
