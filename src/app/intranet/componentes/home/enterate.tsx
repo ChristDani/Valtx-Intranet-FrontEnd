bf518842eae860c8a8b529880094ba52e103274d
gianfranco vives <gianfranco.vives.03@gmail.com>
2024-03-04 15:15:46 -0500

    se creó componente entérate

---
+++ src/app/intranet/componentes/home/enterate.tsx
@@ -0,0 +1,23 @@
+export const Enterate = () =>{
+
+    return(
+        <div className="flex flex-col p-4">
+            <div className="flex justify-between align-bottom font-bold">
+                <h1 className="text-lg global-main-text"> Entérate </h1>
+                <span className="global-secondary-text">Ver todos</span>
+            </div>
+
+            {/** Contenido */}
+            <div className="flex flex-col rounded-2xl justify-between">
+
+                <div className="w-full h-40 bg-[red] rounded-2xl mt-3">
+
+                </div>
+                <div className="w-full h-40 bg-[red] rounded-2xl mt-4">
+
+                </div>
+                
+            </div>
+        </div>
+    );
+}
