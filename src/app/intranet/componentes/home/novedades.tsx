7ad23abc2dd72cb5b9fe95a2f0fbb9c2ef412d5a
gianfranco vives <gianfranco.vives.03@gmail.com>
2024-03-04 15:14:33 -0500

    Se creo componente de novedades

---
+++ src/app/intranet/componentes/home/novedades.tsx
@@ -0,0 +1,16 @@
+export const Novedades = () =>{
+
+    return(
+        <div className="flex flex-col">
+            <div className="flex justify-between align-bottom pt-4 font-bold">
+                <h1 className="text-lg global-main-text"> Novedades </h1>
+                <span className="global-secondary-text">Ver todos</span>
+            </div>
+
+            {/** Contenido */}
+            <div className="w-full h-96 bg-[#1f2f79] rounded-2xl mt-3">
+                
+            </div>
+        </div>
+    );
+}
