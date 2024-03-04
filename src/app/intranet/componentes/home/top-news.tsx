53f90762a2a0aa3e3d8219b8bec87cab466ddba8
gianfranco vives <gianfranco.vives.03@gmail.com>
2024-03-04 15:15:23 -0500

    el componente de top-news se adaptó a la nueva estructura

--- src/app/intranet/componentes/home/top-news.tsx
+++ src/app/intranet/componentes/home/top-news.tsx
@@ -3,22 +3,24 @@ const TopNews = () =>{
 
     return(
         
-        <>
+        <div className="flex flex-col">
+            <div className="flex justify-between align-bottom pt-4 font-bold">
+                <h1 className="global-main-text text-lg"> Valtx news </h1>
+                <span className="global-secondary-text">Ver todos</span>
+            </div>
+            <div className="flex flex-row w-full h-64">
 
-            <div className="flex w-full h-[50%] p-2">
+                <div className="w-1/2 h-[100%] p-4 pl-0">
+                    <div className="h-full rounded-2xl bg-white bg-cover" style={{ backgroundImage: "url(https://www.valtx.pe/img/nosotros/certificaciones-ISO-3.png)"}}></div>
+                </div>
 
-                <div className="w-full h-[100%] rounded-2xl bg-white bg-cover" style={{ backgroundImage: "url(https://www.valtx.pe/img/nosotros/certificaciones-ISO-3.png)"}}></div>
+                <div className="w-1/2 h-[100%] p-4 pr-0">
+                    <div className="h-full rounded-2xl bg-white bg-contain" style={{ backgroundImage: "url(https://i.ytimg.com/vi/RykPB87_PAQ/maxresdefault.jpg)"}}></div>
+                </div>
 
             </div>
-
-            <div className="w-full h-[50%] p-2">
-
-                <div className="w-full h-[100%] rounded-2xl bg-white bg-contain" style={{ backgroundImage: "url(https://i.ytimg.com/vi/RykPB87_PAQ/maxresdefault.jpg)"}}></div>
-
-            </div>
-
-        </>
-
+        </div>
+            
     );
 }
 
