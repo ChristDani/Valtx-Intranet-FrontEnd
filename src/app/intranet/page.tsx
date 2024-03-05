9a955c8dbee6e0b20c36a237f9da523e53de79e7
gianfranco vives <gianfranco.vives.03@gmail.com>
2024-03-05 10:23:19 -0500

    Se añadió sección de blog

--- src/app/intranet/page.tsx
+++ src/app/intranet/page.tsx
@@ -4,6 +4,9 @@ import TopNews from './componentes/home/top-news';
 import Events from './componentes/home/events';
 import Birthdays from './componentes/home/birthdays';
 import Articles from './componentes/home/articles';
+import { Novedades } from './componentes/home/novedades';
+import { Enterate } from './componentes/home/enterate';
+import { Documentacion } from './componentes/home/documentacion';
 
 export default function IntranetPage(){
 
@@ -12,50 +15,32 @@ export default function IntranetPage(){
             <div className="w-full">
 
             {/** Parte superior del home - Banner principal y Noticias **/}
-            <div className="main-container">
+            <div className="flex w-full flex-row">
+                <div className="flex flex-col w-3/4 mt-4">
 
-                    <div className="basis-1/2 flex grow p-6">
-                        
-                        <HomeMainBanner></HomeMainBanner>
+                    <HomeMainBanner></HomeMainBanner>
 
-                    </div>
+                    <TopNews></TopNews>
 
-                    <div className="flex flex-col justify-between basis-1/4 p-2">
+                    <Novedades></Novedades>
 
-                        <TopNews></TopNews>
+                </div>
+                <div className="w-1/4 pl-7"> 
 
-                    </div>
+                    <Enterate></Enterate>
+
+                    <Events></Events>
+
+                    <Documentacion></Documentacion>
+
+                </div>
 
             </div>
 
-            {/** Segunda sección del home - Eventos y Cumpleaños **/}
-            <div className="main-container">
+            {/** Nuestro Blog **/}
+            <div className="w-full mt-4">
 
-                    <div className="flex flex-col w-[50%]  p-4">
-
-                        <div className="w-full min-w-full h-48 rounded-2xl bg-white p-4">
-
-                            <Events></Events>
-
-                        </div>
-                        
-                        <div className="w-full min-w-full grow h-36 rounded-2xl bg-white p-4 mt-4">
-
-                            <Birthdays></Birthdays>
-
-                        </div>
-
-                    </div>
-
-                    <div className="flex grow p-4">
-
-                        <div className="w-full min-w-full h-auto rounded-2xl bg-white"> 
-
-                            <Articles></Articles>
-
-                        </div>
-
-                    </div>
+                <Articles></Articles>
 
             </div>
 
