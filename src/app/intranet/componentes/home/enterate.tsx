4edf4600b3e40230ab06512c34788c7eafa050ba
gianfranco vives <gianfranco.vives.03@gmail.com>
2024-03-05 10:24:38 -0500

    Se añadieron imagenes

--- src/app/intranet/componentes/home/enterate.tsx
+++ src/app/intranet/componentes/home/enterate.tsx
@@ -1,8 +1,8 @@
 export const Enterate = () =>{
 
     return(
-        <div className="flex flex-col p-4">
-            <div className="flex justify-between align-bottom font-bold">
+        <div className="flex flex-col">
+            <div className="flex justify-between align-bottom font-bold mt-4">
                 <h1 className="text-lg global-main-text"> Entérate </h1>
                 <span className="global-secondary-text">Ver todos</span>
             </div>
@@ -10,11 +10,11 @@ export const Enterate = () =>{
             {/** Contenido */}
             <div className="flex flex-col rounded-2xl justify-between">
 
-                <div className="w-full h-40 bg-[red] rounded-2xl mt-3">
-
+                <div className="w-full h-40 rounded-2xl mt-3 overflow-hidden">
+                    <img src="https://ii.ct-stc.com/3/logos/empresas/2022/08/22//valtx181622541thumbnail.jpg"></img>
                 </div>
-                <div className="w-full h-40 bg-[red] rounded-2xl mt-4">
-
+                <div className="w-full h-40 bg-[red] rounded-2xl mt-4 overflow-hidden">
+                    <img src="https://www.valtx.pe/img/share/blog.jpg"></img>
                 </div>
                 
             </div>
