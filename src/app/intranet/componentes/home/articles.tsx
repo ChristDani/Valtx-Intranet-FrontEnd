ffd9ca27a7deafa6f504c731e3c8524a66875ee8
gianfranco vives <gianfranco.vives.03@gmail.com>
2024-03-05 10:21:48 -0500

    El componente de artículos fue adaptado para la nueva estructura

--- src/app/intranet/componentes/home/articles.tsx
+++ src/app/intranet/componentes/home/articles.tsx
@@ -1,48 +1,92 @@
+import Link from "next/link";
+
 const Articles = () =>{
 
     return(
 
         <>
-        <div className="flex m-4 p-4 bg-gray-100 rounded-2xl justify-center"> 
-        <h2 className="text-xl font-bold text-cyan-900">Atrículos recientes</h2>
-        </div>
+        <div className="flex justify-between align-bottom font-bold mt-4">
+                <h1 className="text-lg global-main-text"> Nuestro blog </h1>
+                <span className="global-secondary-text">Ver todos</span>
+            </div>
+        <div className="flex justify-between">
 
-        <div className="flex mx-6 my-8">
-            <img src="https://rumboeconomico.com/wp-content/uploads/2023/04/Foto-equipo-tgestiona-y-Valtx-.jpg" className=" rounded-xl w-40"></img>
-            <div className="ml-6">
-                <div className="flex">
-                    <h3 className="text-gray-600 font-bold">Marzo 2023</h3>
-                    <div className="rounded-full w-3 h-3 ml-4 bg-sky-500 self-center"></div>
-                </div>
-                <br></br>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit harum et error nulla odio nisi cum dolore eos voluptates, modi.</p>
-            </div>
-        </div>
-        
-        <div className="flex mx-6 my-8">
-            <img src="https://ii.ct-stc.com/3/logos/empresas/2022/08/18//valtx201757048thumbnail.jpg" className=" rounded-xl w-40"></img>
-            <div className="ml-6">
-                <div className="flex">
-                    <h3 className="text-gray-600 font-bold">Noviembre 2023</h3>
-                    <div className="rounded-full w-3 h-3 ml-4 bg-sky-500 self-center"></div>
-                </div>
-                <br></br>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit harum et error nulla odio nisi cum dolore eos voluptates, modi.</p>
-            </div>
-        </div>
-        
-        <div className="flex mx-6 my-8">
-            <img src="https://ii.ct-stc.com/3/logos/empresas/2022/08/22//valtx181622541thumbnail.jpg" className=" rounded-xl w-40"></img>
-            <div className="ml-6">
-                <div className="flex">
-                    <h3 className="text-gray-600 font-bold">Junio 2023</h3>
-                    <div className="rounded-full w-3 h-3 ml-4 bg-sky-500 self-center"></div>
-                </div>
-                <br></br>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit harum et error nulla odio nisi cum dolore eos voluptates, modi.</p>
-            </div>
-        </div>
+            {/** Item de blog */}
+            <div className="flex flex-col bg-white my-8 rounded-xl w-96">
 
+                {/** Sección de la imágen */}
+                <div className="bg-[red] h-52 overflow-hidden rounded-t-xl w-full">
+                    <img src="https://rumboeconomico.com/wp-content/uploads/2023/04/Foto-equipo-tgestiona-y-Valtx-.jpg"></img>
+                </div>
+            
+                {/** contenido */}
+                <div className="ml-6">
+
+                    <div className="flex mt-2">
+                        <h3 className="font-bold global-main-text">Microservicios en desarrollo de software</h3>
+                        <div className="rounded-full h-auto ml-4 bg-sky-500 self-center"></div>
+                    </div>
+                    <br></br>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit harum et error nulla odio nisi cum dolore eos voluptates, modi...</p>
+                </div>
+                
+                {/** footer */}
+                <div className="flex mt-9 justify-end mr-2">
+                    <Link href={''} className="global-secondary-text">Ver detalles -{'>'}</Link>
+                </div>
+            </div>
+            
+            {/** Item de blog */}
+            <div className="flex flex-col bg-white my-8 rounded-xl w-96">
+
+                {/** Sección de la imágen */}
+                <div className="bg-[red] h-52 overflow-hidden rounded-t-xl w-full">
+                    <img src="https://rumboeconomico.com/wp-content/uploads/2023/04/Foto-equipo-tgestiona-y-Valtx-.jpg"></img>
+                </div>
+            
+                {/** contenido */}
+                <div className="ml-6">
+
+                    <div className="flex mt-2">
+                        <h3 className="font-bold global-main-text">5 estrategias CIO para adoptar IA generativa en los procesos</h3>
+                        <div className="rounded-full h-auto ml-4 bg-sky-500 self-center"></div>
+                    </div>
+                    <br></br>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit harum et error nulla odio nisi cum dolore eos voluptates, modi...</p>
+                </div>
+                
+                {/** footer */}
+                <div className="flex mt-9 justify-end mr-2">
+                    <Link href={''} className="global-secondary-text">Ver detalles -{'>'}</Link>
+                </div>
+            </div>
+
+            {/** Item de blog */}
+            <div className="flex flex-col bg-white my-8 rounded-xl w-96">
+
+                {/** Sección de la imágen */}
+                <div className="bg-[red] h-52 overflow-hidden rounded-t-xl w-full">
+                    <img src="https://rumboeconomico.com/wp-content/uploads/2023/04/Foto-equipo-tgestiona-y-Valtx-.jpg"></img>
+                </div>
+            
+                {/** contenido */}
+                <div className="ml-6">
+
+                    <div className="flex mt-2">
+                        <h3 className="font-bold global-main-text">Seguridad en el Desarrollo de Software: Mejores Prácticas y Estrategias</h3>
+                        <div className="rounded-full h-auto ml-4 bg-sky-500 self-center"></div>
+                    </div>
+                    <br></br>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit harum et error nulla odio nisi cum dolore eos voluptates, modi...</p>
+                </div>
+                
+                {/** footer */}
+                <div className="flex mb-3 mr-3 mt-9 justify-end">
+                    <Link href={''} className="global-secondary-text">Ver detalles -{'>'}</Link>
+                </div>
+            </div>
+            
+        </div>
     </>
 
     );
