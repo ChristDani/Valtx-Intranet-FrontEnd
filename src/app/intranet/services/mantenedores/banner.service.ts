28c22fb61b1435bf41ae99349a41122927157791
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-08 14:44:38 -0500

    Obtención de token y autenticación para la solicitud

--- src/app/intranet/services/mantenedores/banner.service.ts
+++ src/app/intranet/services/mantenedores/banner.service.ts
@@ -1,19 +1,32 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import axiosClient from './axios';
+import { getCookie } from '../get-cookie.service';
+import tokenAuth from './token.service';
+
+
+const token = getCookie('token') || '';
+
+tokenAuth(token);
+
 
 export const bannerServices = {
-    async getBanners() {
+
+
+    async getBanners() { 
+        
         const { data } = await axiosClient.post('api/v1/banner/getBannerList', {
             "inumero_pagina": 0,
             "itotal_pagina": 10,
             "vtitulo": "",
             "iid_estado_registro": 1
         });
+
         return data;
+
     },
     
     async getBanner(id:any) {
-        const res = await axios.post(`api/v1/banner/getBannerId?iid_banner=${id}`)
+        const res = await axios.post(`api/v1/banner/getBannerId?iid_banner=${id}`);
     },
     
     async setBanner() {
