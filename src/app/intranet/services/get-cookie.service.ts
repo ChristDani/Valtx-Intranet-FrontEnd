29f040ffa79dae6edf9a6f3afa0a5df626b70c8b
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-08 14:45:06 -0500

    Servicio para obtener cookies

---
+++ src/app/intranet/services/get-cookie.service.ts
@@ -0,0 +1,7 @@
+export const getCookie = (name: string) =>{
+        const cookie = document.cookie
+            .split('; ')
+            .find(cookie => cookie.startsWith(name + '='));
+    
+        return cookie ? decodeURIComponent(cookie.split('=')[1]) : undefined;
+}
