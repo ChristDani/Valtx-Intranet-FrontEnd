ec0ae4b251a7e8d7a93ceb20703147e0663b10bb
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-11 15:43:15 -0500

    Se obtiene la información del backend

--- src/app/intranet/componentes/home/documentacion.tsx
+++ src/app/intranet/componentes/home/documentacion.tsx
@@ -1,4 +1,30 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { documentacionServices } from "../../services/mantenedores/document.service";
+
 export const Documentacion = () =>{
+
+    const [images, setImages] = useState<string[]>([]);
+
+    useEffect(()=>{
+        getAllDocumentation();
+        console.log(images);
+    }, []);
+
+    
+    useEffect(()=>{
+        console.log(images)
+      }, [images])
+  
+
+    const getAllDocumentation = async () =>{
+        const docum = await documentacionServices.getDocumentaciones(1,10);
+        const documList = docum.data;
+        const imagesToAdd = documList.map((docum:any) => docum.vimagen);
+        setImages(imagesToAdd);
+
+    }
 
     return(
         <div className="flex flex-col">
@@ -7,16 +33,19 @@ export const Documentacion = () =>{
             </div>
 
             {/** Contenido */}
-            <div className="flex flex-col rounded-2xl justify-between">
+            <div className="cursor-pointer flex flex-col rounded-2xl justify-between">
 
-                <div className="w-full h-40 bg-[red] rounded-2xl mt-3">
-
-                </div>
-                <div className="w-full h-40 bg-[red] rounded-2xl mt-4">
+                <div className="bg-cover cursor-pointer  w-full h-40 rounded-2xl mt-3"
+                        style={{ backgroundImage: `url(${`/images/${images[0]}`})` }}>
 
                 </div>
+                <div className="bg-cover cursor-pointer  w-full h-40 rounded-2xl mt-4"
+                        style={{ backgroundImage: `url(${`/images/${images[1]}`})` }}>
 
-                <div className="w-full h-40 bg-[red] rounded-2xl mt-4">
+                </div>
+
+                <div className="bg-cover cursor-pointer  w-full h-40 rounded-2xl mt-4"
+                        style={{ backgroundImage: `url(${`/images/${images[2]}`})` }}>
 
                 </div>
                 
