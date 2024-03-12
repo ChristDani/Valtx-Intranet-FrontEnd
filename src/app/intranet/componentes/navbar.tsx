c7cfaa464656bbccee27d74d223f29dd6872be8a
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-11 21:16:52 -0500

    Se agregó un salto de línea para el nombre del usuario

--- src/app/intranet/componentes/navbar.tsx
+++ src/app/intranet/componentes/navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
 
     return (
 
-        <div className="flex items-center h-16 justify-between my-6 text-white top-0 w-full">
+        <div className="flex items-center h-16 justify-between my-6 mr-4 text-white top-0 w-full">
             <div className="relative flex">
                 <svg width="165" height="48" viewBox="0 0 165 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M14.7632 0H0L14.7632 47.1842H32.1316L47.1842 0H32.1316L23.4474 27.7895L14.7632 0Z" fill="#0C3587" />
@@ -27,7 +27,7 @@ export default function Navbar() {
             </div>
             <div className="flex items-center">
                 <img src="https://t3.ftcdn.net/jpg/04/77/87/44/360_F_477874414_kSQ9ip26804g8B3ItYsh5XsjNRgqf693.jpg" alt="Foto de perfil" className="w-10 h-10 rounded-full mr-2" />
-                {/* <h3 className="text-black mr-2">Hola, {localStorage.getItem("userName")} {localStorage.getItem("userFirstLastName")} {localStorage.getItem("userSecondLastName")}</h3> */}
+                <h3 className="text-black mr-2">Hola, <br></br> <b>{localStorage.getItem("userName")} {localStorage.getItem("userFirstLastName")} {localStorage.getItem("userSecondLastName")}</b></h3> 
                 <div className="flex pt-4 h-[50px] rounded-md">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-black" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M10 12l-5.707-5.707a1 1 0 0 1 1.414-1.414L10 9.586l4.293-4.293a1 1 0 0 1 1.414 1.414L10 12z" clipRule="evenodd" />
