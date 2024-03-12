00073eca3a938ba65e941cf73cd4c42f97f6f86d
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-12 14:47:25 -0500

    Se dio estilo al nombre de usuario

--- src/app/intranet/componentes/navbar.tsx
+++ src/app/intranet/componentes/navbar.tsx
@@ -26,10 +26,10 @@ export default function Navbar() {
                 </svg>
             </div>
             <div className="flex items-center">
-                <img src="https://t3.ftcdn.net/jpg/04/77/87/44/360_F_477874414_kSQ9ip26804g8B3ItYsh5XsjNRgqf693.jpg" alt="Foto de perfil" className="w-10 h-10 rounded-full mr-2" />
-                <h3 className="text-black mr-2">Hola, <br></br> <b>{localStorage.getItem("userName")} {localStorage.getItem("userFirstLastName")} {localStorage.getItem("userSecondLastName")}</b></h3> 
+                <img className="w-10 h-10 bg-[#0C3587] rounded-full mr-4" />
+                <h3 className="text-[#0C3587] mr-2">Hola, <br></br> <b>{localStorage.getItem("userName")} {localStorage.getItem("userFirstLastName")} {localStorage.getItem("userSecondLastName")}</b></h3> 
                 <div className="flex pt-4 h-[50px] rounded-md">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-black" viewBox="0 0 20 20" fill="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-[#0C3587]" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M10 12l-5.707-5.707a1 1 0 0 1 1.414-1.414L10 9.586l4.293-4.293a1 1 0 0 1 1.414 1.414L10 12z" clipRule="evenodd" />
                     </svg>
                 </div>
