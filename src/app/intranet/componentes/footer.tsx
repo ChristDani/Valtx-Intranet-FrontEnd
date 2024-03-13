baaf0f168a4e8a4e330be11f3bd322dd3cbdd819
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-13 01:58:26 -0500

    Se añadió método para ir a Link y se añadieron íconos

--- src/app/intranet/componentes/footer.tsx
+++ src/app/intranet/componentes/footer.tsx
@@ -1,7 +1,15 @@
+'use client'
 export const Footer = () =>{
+
+    const goToLink = (vlink: string) =>{
+        if(vlink == null || vlink == '') return;
+        window.open(vlink);
+      }
+
+
     return(
 
-        <div className="flex bg-[#1f2f79] h-60 justify-center w-full">
+        <div className="flex bg-[#0C3587] h-60 justify-center w-full">
             <div className="flex h-full justify-between pt-9 w-[80%]">
                 
                 {/** contenedor de sistemas de apoyo */}
@@ -36,14 +44,17 @@ export const Footer = () =>{
                     {/** contenido */}
                     <div className="flex mt-4">
 
-                        <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
-
+                        <div className="flex h-16 justify-center w-20 bg-white rounded-xl mr-2 bg-contain">
+                            <img src="https://www.valtx.pe/img/nosotros/certificaciones-ISO-1.png"
+                                className="h-[80%] m-auto w-[80%]"></img>
                         </div>
-                        <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
-
+                        <div className="flex h-16 justify-center w-20 bg-white rounded-xl mr-2 bg-contain">
+                            <img src="https://www.valtx.pe/img/nosotros/certificaciones-ISO-3.png"
+                                className="h-[80%] m-auto w-[80%]"></img>
                         </div>
-                        <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
-
+                        <div className="flex h-16 justify-center w-20 bg-white rounded-xl mr-2 bg-contain">
+                            <img src="https://www.valtx.pe/img/nosotros/certificaciones-ISO-1.png"
+                                className="h-[80%] m-auto w-[80%]"></img>
                         </div>
                     </div>
 
@@ -59,15 +70,22 @@ export const Footer = () =>{
                     {/** contenido */}
                     <div className="flex mt-4">
 
-                        <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
+                        <div className="bg-contain cursor-pointer h-16 w-16 rounded-xl mr-2"
+                            style={{backgroundImage:'url(icons/social-media/1.png)'}}
+                            onClick={()=>{goToLink('https://www.facebook.com/ValtxPeru?locale=ms_MY')}}>
 
                         </div>
-                        <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
+                        <div className="bg-contain cursor-pointer h-16 w-16 rounded-xl mr-2"
+                            style={{backgroundImage:'url(icons/social-media/2.png)'}}
+                            onClick={()=>{goToLink('https://www.valtx.pe')}}>
 
                         </div>
-                        <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
+                        <div className="bg-contain cursor-pointer h-16 w-16 rounded-xl mr-2"
+                            style={{backgroundImage:'url(icons/social-media/3.png)'}}
+                            onClick={()=>{goToLink('https://www.youtube.com/@valtx4640')}}>
 
                         </div>
+                        
                     </div>
 
                 </div>
