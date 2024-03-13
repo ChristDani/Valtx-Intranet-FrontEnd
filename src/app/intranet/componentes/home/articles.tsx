dbe7ee8318de0ae4a00a90ebf092ad2163018189
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-13 02:00:18 -0500

    se obtiene data de blog

--- src/app/intranet/componentes/home/articles.tsx
+++ src/app/intranet/componentes/home/articles.tsx
@@ -1,6 +1,28 @@
+'use client'
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { blogServices } from "../../services/mantenedores/blogs.service";
 
 const Articles = () =>{
+
+    const [articles, setArticles] = useState([{vtitulo:'', vtextobreve:'', vlink:'', vimagen:''}]); 
+
+{}
+    useEffect(()=>{
+        getArticles();
+    }, []);
+
+    useEffect(()=>{
+        
+    }, [articles])
+
+    const getArticles = async () =>{
+        const articlesRes = await blogServices.getList(1,10);
+        const articlesList = articlesRes.data;
+        console.log(articlesRes);
+        setArticles(articlesList);
+    }
 
     return(
 
@@ -11,81 +33,28 @@ const Articles = () =>{
             </div>
         <div className="flex justify-between">
 
-            {/** Item de blog */}
-            <div className="flex flex-col bg-white my-8 rounded-xl w-96">
+        {articles.map((article, index) => (
 
-                {/** Sección de la imágen */}
+            <div className="flex flex-col bg-white my-8 rounded-xl w-[32%]">
+
                 <div className="bg-[red] h-52 overflow-hidden rounded-t-xl w-full">
-                    <img src="https://rumboeconomico.com/wp-content/uploads/2023/04/Foto-equipo-tgestiona-y-Valtx-.jpg"></img>
+                    <img src={`/images/${article.vimagen}`}></img>
                 </div>
-            
-                {/** contenido */}
                 <div className="ml-6">
 
                     <div className="flex mt-2">
-                        <h3 className="font-bold global-main-text">Microservicios en desarrollo de software</h3>
+                        <h3 className="font-bold text-xl text-[#00245F]">{article.vtitulo}</h3>
                         <div className="rounded-full h-auto ml-4 bg-sky-500 self-center"></div>
                     </div>
                     <br></br>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit harum et error nulla odio nisi cum dolore eos voluptates, modi...</p>
+                    <p className="text-[#00245F]">{article.vtextobreve}</p>
                 </div>
                 
-                {/** footer */}
-                <div className="flex mt-9 justify-end mr-2">
+                <div className="flex mt-9 justify-end mr-3 mb-3">
                     <Link href={''} className="global-secondary-text">Ver detalles -{'>'}</Link>
                 </div>
             </div>
-            
-            {/** Item de blog */}
-            <div className="flex flex-col bg-white my-8 rounded-xl w-96">
-
-                {/** Sección de la imágen */}
-                <div className="bg-[red] h-52 overflow-hidden rounded-t-xl w-full">
-                    <img src="https://rumboeconomico.com/wp-content/uploads/2023/04/Foto-equipo-tgestiona-y-Valtx-.jpg"></img>
-                </div>
-            
-                {/** contenido */}
-                <div className="ml-6">
-
-                    <div className="flex mt-2">
-                        <h3 className="font-bold global-main-text">5 estrategias CIO para adoptar IA generativa en los procesos</h3>
-                        <div className="rounded-full h-auto ml-4 bg-sky-500 self-center"></div>
-                    </div>
-                    <br></br>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit harum et error nulla odio nisi cum dolore eos voluptates, modi...</p>
-                </div>
-                
-                {/** footer */}
-                <div className="flex mt-9 justify-end mr-2">
-                    <Link href={''} className="global-secondary-text">Ver detalles -{'>'}</Link>
-                </div>
-            </div>
-
-            {/** Item de blog */}
-            <div className="flex flex-col bg-white my-8 rounded-xl w-96">
-
-                {/** Sección de la imágen */}
-                <div className="bg-[red] h-52 overflow-hidden rounded-t-xl w-full">
-                    <img src="https://rumboeconomico.com/wp-content/uploads/2023/04/Foto-equipo-tgestiona-y-Valtx-.jpg"></img>
-                </div>
-            
-                {/** contenido */}
-                <div className="ml-6">
-
-                    <div className="flex mt-2">
-                        <h3 className="font-bold global-main-text">Seguridad en el Desarrollo de Software: Mejores Prácticas y Estrategias</h3>
-                        <div className="rounded-full h-auto ml-4 bg-sky-500 self-center"></div>
-                    </div>
-                    <br></br>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit harum et error nulla odio nisi cum dolore eos voluptates, modi...</p>
-                </div>
-                
-                {/** footer */}
-                <div className="flex mb-3 mr-3 mt-9 justify-end">
-                    <Link href={''} className="global-secondary-text">Ver detalles -{'>'}</Link>
-                </div>
-            </div>
-            
+        ))}
         </div>
     </>
 
