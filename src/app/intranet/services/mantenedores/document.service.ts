4f3b2cc09766937d8d69f3d8724ea8a952f5d0ee
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-18 10:30:20 -0500

    se creó servicio para guardar

--- src/app/intranet/services/mantenedores/document.service.ts
+++ src/app/intranet/services/mantenedores/document.service.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from 'axios'
 import axiosClient from '../axios.service';
 import { getCookie } from '../get-cookie.service';
 import tokenAuth from '../token.service';
-import { DocumentationResponseDTO } from '../../interfaces/documentacion.response.dto';
+import { Documentation, DocumentationResponseDTO } from '../../interfaces/documentacion.response.dto';
 
 
 const token = getCookie('token') || '';
@@ -16,7 +16,8 @@ export const documentacionServices = {
             "inumero_pagina": pageNumber-1, // 0
             "itotal_pagina": itemsPerPage, // 10
             "vtitulo": titulo, // ""
-            "iid_estado_registro": state // -1
+            "iid_estado_registro": state, // -1
+            "order": "asc"
         });
 
         return data;
@@ -26,12 +27,32 @@ export const documentacionServices = {
         const res = await axiosClient.post(`api/v1/documentacion/getDocumId?iid_documentacion=${id}`)
     },
     
-    async create() {
-        const res = await axiosClient.post('api/v1/documentacion/setDocum')
+    async create(document: Documentation, image?: File) {
+
+        tokenAuth(token,'multipart/form-data');
+
+        const formData = new FormData();
+        if(image) formData.append('image', image);
+        formData.append('iid_documentacion', '0');
+        formData.append('vtitulo', document.vtitulo);
+        formData.append('vtextobreve', document.vtextobreve);
+        formData.append('vlink', document.vlink);
+        formData.append('vredireccion', document.vredireccion);
+        formData.append('iorden', document.iorden.toString()); 
+        formData.append('dfecha', document.dfecha);
+        formData.append('iid_estado_registro', document.iid_estado_registro.toString());
+        formData.append('storage', '/documentacion');
+        formData.append('vdescripcion_estado', document.vdescripcion_estado);
+        
+
+
+        const res = await axiosClient.post('api/v1/documentacion/setDocum', formData);
     },
     
     async update() {
-        const res = await axiosClient.post('api/v1/documentacion/updateDocum')
+
+
+        const res = await axiosClient.post('api/v1/documentacion/updateDocum');
     },
     
     async delete(id:any) {
