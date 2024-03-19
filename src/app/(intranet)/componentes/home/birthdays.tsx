dfe7b087da2ca5d3850df3afbd46afe9b6f6b4af
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-19 15:58:48 -0500

    Se eliminaron rutas/archivos no utilizados

--- src/app/(intranet)/componentes/home/birthdays.tsx
+++
@@ -1,7 +0,0 @@
-const Birthdays = () =>{
-    return(
-        <h1 className='text-xl font-bold text-cyan-900'>Cumpleaños</h1>
-    );
-}
-
-export default Birthdays;
--- src/app/(intranet)/pages/areas/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function AreasPage(){
-    return(
-        <div>Areassss</div>
-    );
-}
