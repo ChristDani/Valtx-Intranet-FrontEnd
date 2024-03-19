a508d97ba22cf718066fd9a7764723c193b48cf2
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-03-18 21:51:25 -0500

    se añadieron íconos

--- src/app/intranet/componentes/footer.tsx
+++ src/app/intranet/componentes/footer.tsx
@@ -22,13 +22,20 @@ export const Footer = () =>{
                     <div className="flex mt-4">
 
                         <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
-
+                            <img src="/icons/sistemas-apoyo/1.png"
+                                    className="h-[100%] m-auto w-[100%]"></img>
                         </div>
                         <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
-
+                             <img src="/icons/sistemas-apoyo/2.png"
+                                    className="h-[100%] m-auto w-[100%]"></img>
                         </div>
                         <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
-
+                             <img src="/icons/sistemas-apoyo/3.png"
+                                    className="h-[100%] m-auto w-[100%]"></img>
+                        </div>
+                        <div className="h-16 w-20 bg-gray-300 rounded-xl mr-2">
+                             <img src="/icons/sistemas-apoyo/4.png"
+                                    className="h-[100%] m-auto w-[100%]"></img>
                         </div>
                     </div>
 
@@ -73,19 +80,27 @@ export const Footer = () =>{
                         <div className="bg-contain cursor-pointer h-16 w-16 rounded-xl mr-2"
                             style={{backgroundImage:'url(icons/social-media/1.png)'}}
                             onClick={()=>{goToLink('https://www.facebook.com/ValtxPeru?locale=ms_MY')}}>
-
+                                <img src="/icons/social-media/1.png"
+                                className="h-[80%] m-auto w-[80%]"></img>
                         </div>
                         <div className="bg-contain cursor-pointer h-16 w-16 rounded-xl mr-2"
                             style={{backgroundImage:'url(icons/social-media/2.png)'}}
                             onClick={()=>{goToLink('https://www.valtx.pe')}}>
-
+                                <img src="/icons/social-media/2.png"
+                                    className="h-[80%] m-auto w-[80%]"></img>
                         </div>
                         <div className="bg-contain cursor-pointer h-16 w-16 rounded-xl mr-2"
                             style={{backgroundImage:'url(icons/social-media/3.png)'}}
                             onClick={()=>{goToLink('https://www.youtube.com/@valtx4640')}}>
-
+                                    <img src="/icons/social-media/5.png"
+                                    className="h-[80%] m-auto w-[80%]"></img>
                         </div>
-                        
+                        <div className="bg-contain cursor-pointer h-16 w-16 rounded-xl mr-2"
+                            style={{backgroundImage:'url(icons/social-media/3.png)'}}
+                            onClick={()=>{goToLink('https://www.youtube.com/@valtx4640')}}>
+                                    <img src="/icons/social-media/3.png"
+                                    className="h-[80%] m-auto w-[80%]"></img>
+                        </div>
                     </div>
 
                 </div>
