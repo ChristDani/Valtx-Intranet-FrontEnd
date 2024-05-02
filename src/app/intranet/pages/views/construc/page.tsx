12765d03c81b8cd97a09c78a5b48a65d7ce387b7
RoyerAI <racostai@unprg.edu.pe>
2024-05-02 16:11:31 -0500

    error

--- src/app/intranet/pages/views/construc/page.tsx
+++ src/app/intranet/pages/views/construc/page.tsx
@@ -3,7 +3,7 @@ export const Construction = () => {
         <div className="w-full h-full ">
             <img src="/construction.svg" className="h-[350px] mx-auto" alt="" />
             <div className="text-center mt-10 text-3xl font-bold">¡Pagina en construcción!</div>
-            <div className="text-center mt-10 text-xl font-bold">No vuelva <Pronto></Pronto></div>
+            <div className="text-center mt-10 text-xl font-bold">No vuelva Pronto</div>
         </div>
     )
 }
