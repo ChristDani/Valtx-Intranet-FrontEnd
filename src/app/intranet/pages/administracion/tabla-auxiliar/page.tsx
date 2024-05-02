4a2ab818a1ce7b938431a619b7966e2c1e97e301
RoyerAI <racostai@unprg.edu.pe>
2024-05-02 16:10:40 -0500

    pagina en construccion

--- src/app/intranet/pages/administracion/tabla-auxiliar/page.tsx
+++ src/app/intranet/pages/administracion/tabla-auxiliar/page.tsx
@@ -1,11 +1,11 @@
+import { Construction } from "../../views/construc/page";
+
 const AuxPage = () =>{
     
     return(
 
-        <div className="mt-12 pt-6 ml-80">
-            <div>
-                <h1>Tablas Auxiliares</h1>
-            </div>
+        <div className="w-full">
+            <Construction />
         </div>
 
     );
---
+++ src/app/intranet/pages/views/construc/page.tsx
@@ -0,0 +1,9 @@
+export const Construction = () => {
+    return (
+        <div className="w-full h-full ">
+            <img src="/construction.svg" className="h-[350px] mx-auto" alt="" />
+            <div className="text-center mt-10 text-3xl font-bold">¡Pagina en construcción!</div>
+            <div className="text-center mt-10 text-xl font-bold">No vuelva <Pronto></Pronto></div>
+        </div>
+    )
+}
