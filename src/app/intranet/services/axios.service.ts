921d694ec41d25a176fbabfb3d4e909308c82e26
Jotace011 <jotace.026.3@gmail.com>
2024-05-29 12:17:32 -0500

    cambio-test

--- src/app/intranet/services/axios.service.ts
+++ src/app/intranet/services/axios.service.ts
@@ -3,10 +3,12 @@ import axios from 'axios';
 /**
  * http://localhost:4000/graphql
  * http://localhost:4000/post-image
+ * https://intranetconexion.valtx.pe:9021/api/v1
+ * 'http://172.13.34.99:4000/api/v1'
  */
 
 const axiosClient = axios.create({
-  baseURL: 'https://intranetconexion.valtx.pe:9021/api/v1',
+  baseURL: 'http://172.13.34.99:4000/api/v1',
 });
 
 export default axiosClient;
