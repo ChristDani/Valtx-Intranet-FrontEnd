17c25be4b1d27c5ac41c558830b98cabd50e98ef
RoyerAI <racostai@unprg.edu.pe>
2024-06-12 18:22:27 -0500

    loader

--- src/app/intranet/layout.tsx
+++ src/app/intranet/layout.tsx
@@ -2,14 +2,12 @@ import Sidebar from "./componentes/Sidebar";
 import Navbar from "./componentes/navbar";
 import "./../globals.css";
 import { Footer } from "./componentes/footer";
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import Loader from "./componentes/loader";
-
-export default function IntranetLayout( {children} : {children: React.ReactNode} ){
+export default function IntranetLayout( {children} : {children: React.ReactNode}){
 
     
     return(
-
             <div className=" flex flex-col justify-center w-full min-h-screen">
 
                 <div className="flex flex-col flex-1 w-[80%] self-center h-auto">
@@ -19,8 +17,10 @@ export default function IntranetLayout( {children} : {children: React.ReactNode}
 
                 {/* Sidebar */}
                 <Sidebar />
-                    <Suspense fallback={<Loader />}/>
+                    <Suspense fallback={<Loader />}>
                         {children}
+                    </Suspense>
+                        
                 </div>
                 <div className="">
                     <Footer></Footer>
